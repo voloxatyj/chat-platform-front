4b41ea9498b017fd57e457ba567e05bfcf10e0b8
Medynskyy Ostap <medinsky_os@groupbwt.com>
2022-09-26 19:27:48 +0300

    Added Mock Conversations & Styled Conversation Sidebar Item

---
+++ src/__mocks__/conversations.ts
@@ -0,0 +1,19 @@
+import { ConversationType } from '../utils/types';
+
+export default [
+  {
+    id: 1,
+    name: 'John Doe',
+    lastMessage: 'How are you?',
+  },
+  {
+    id: 2,
+    name: 'John Doe',
+    lastMessage: 'What are you doing?',
+  },
+  {
+    id: 3,
+    name: 'John Doe',
+    lastMessage: 'You feel well?',
+  },
+] as ConversationType[];
